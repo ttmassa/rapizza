3482e4c54f4d2649e66d91979d8215e065c7087c
Massa Timothée <timothee.massa.perso@gmail.com>
2024-04-14 19:15:59 +0200

    improve pizza card's design

--- src/main/java/com/rapizza/ClientPanel.java
+++ src/main/java/com/rapizza/ClientPanel.java
@@ -9,7 +9,8 @@ import java.util.Vector;
 
 public class ClientPanel extends JPanel {
     private Pizzeria pizzeria;
-    private final int CARD_HEIGHT = 140; 
+    private final int CARD_HEIGHT = 150;
+    private final int CARD_WIDTH = 200;
 
     public ClientPanel(Pizzeria pizzeria) {
         this.pizzeria = pizzeria;
@@ -34,6 +35,7 @@ public class ClientPanel extends JPanel {
         JButton logoutButton = new JButton("Logout");
         configureLinkButton(logoutButton);
 
+        // Add action listener to the logout button
         LogoutButtonListener logoutButtonListener = new LogoutButtonListener(this);
         logoutButton.addActionListener(logoutButtonListener);
 
@@ -48,22 +50,13 @@ public class ClientPanel extends JPanel {
 
         // Main panel
         JPanel mainPanel = new JPanel();
-        int nbrColumns = pizzeria.menu.size();
-
-        mainPanel.setLayout(new GridBagLayout());
-        GridBagConstraints gbc = new GridBagConstraints();
-        // Create gaps between the cards
-        gbc.insets = new Insets(10, 10, 10, 10);
+        mainPanel.setLayout(new FlowLayout(FlowLayout.CENTER)); 
 
         // Create the menu
         for (Pizza pizza : pizzeria.menu) {
             JPanel card = createPizzaCard(pizza.nom, pizza.prixMarge, pizza.ingredients);
-            gbc.gridx = pizzeria.menu.indexOf(pizza) % nbrColumns;
-            gbc.gridy = pizzeria.menu.indexOf(pizza) / nbrColumns;
-            gbc.fill = GridBagConstraints.BOTH; 
-            card.setPreferredSize(new Dimension(card.getPreferredSize().width, CARD_HEIGHT)); 
-
-            mainPanel.add(card, gbc);
+            card.setPreferredSize(new Dimension(CARD_WIDTH, CARD_HEIGHT));
+            mainPanel.add(card);
         }
 
         this.add(mainPanel, BorderLayout.CENTER);
@@ -76,10 +69,6 @@ public class ClientPanel extends JPanel {
         this.add(orderButton, BorderLayout.SOUTH);
     }
 
-    public Pizzeria getPizzeria() {
-        return pizzeria;
-    }
-
     private JPanel createPizzaCard(String title, double price, Vector<Ingredient> ingredients) {
         // Create a new card
         JPanel card = new JPanel();
@@ -88,43 +77,56 @@ public class ClientPanel extends JPanel {
         card.setBackground(Color.WHITE);
         // Add padding to the card
         card.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
-    
+
         // Create the title
         JLabel titleLabel = new JLabel(title + " - " + price + "€");
         titleLabel.setHorizontalAlignment(JLabel.CENTER);
         titleLabel.setForeground(Color.BLACK);
-        titleLabel.setFont(titleLabel.getFont().deriveFont(Font.BOLD, 16)); // Increase font size
+        titleLabel.setFont(titleLabel.getFont().deriveFont(Font.BOLD, 16)); 
         card.add(titleLabel, BorderLayout.NORTH);
-    
+
+        // Add a border below the title
+        titleLabel.setBorder(BorderFactory.createMatteBorder(0, 0, 1, 0, Color.GRAY));
+
+        // Create the main panel
+        JPanel maiPanel = new JPanel();
+        maiPanel.setLayout(new BorderLayout());
+        maiPanel.setBackground(Color.WHITE);
+
         // Create the ingredients panel
         JPanel ingredientsPanel = new JPanel();
         ingredientsPanel.setBackground(Color.WHITE);
-        ingredientsPanel.setLayout(new GridLayout(0, 1)); // Vertical layout for ingredients
+        ingredientsPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
         for (Ingredient ingredient : ingredients) {
             JLabel ingredientLabel = new JLabel(ingredient.nom);
+            ingredientLabel.setFont(ingredientLabel.getFont().deriveFont(Font.ITALIC));
             ingredientsPanel.add(ingredientLabel);
         }
-        // Add border below ingredients section
-        ingredientsPanel.setBorder(BorderFactory.createMatteBorder(1, 0, 0, 0, Color.GRAY));
-        card.add(ingredientsPanel, BorderLayout.CENTER);
-    
+        // No border added to the ingredients panel
+        maiPanel.add(ingredientsPanel, BorderLayout.NORTH);
+
+        // Create a combobox for the pizza size
+        JComboBox<String> sizeComboBox = new JComboBox<>(new String[] { "Naine", "Humaine", "Ogresse" });
+        sizeComboBox.setSelectedIndex(1); 
+        sizeComboBox.setFocusable(false);
+        sizeComboBox.setBackground(Color.WHITE);
+        maiPanel.add(sizeComboBox, BorderLayout.SOUTH);
+
+        card.add(maiPanel, BorderLayout.CENTER);
+
+        // Add space between ingredients and select button
+        maiPanel.setBorder(BorderFactory.createEmptyBorder(0, 0, 10, 0));
+
         // Create a select button
         JButton selectButton = new JButton("Select");
-        selectButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
-        selectButton.setFocusable(false);
-        selectButton.setBorderPainted(false);
-        selectButton.setContentAreaFilled(false);
-        selectButton.setBackground(Color.BLUE); // Set background color to blue
-        selectButton.setForeground(Color.WHITE); // Set text color to white
-        selectButton.setFont(selectButton.getFont().deriveFont(Font.BOLD)); // Make text bold
-        selectButton.setOpaque(true); // Make the button opaque to show background color
+        configureSelectButton(selectButton);
+
         // Add border above select button
-        selectButton.setBorder(BorderFactory.createMatteBorder(1, 0, 0, 0, Color.GRAY));
         card.add(selectButton, BorderLayout.SOUTH);
-    
-        return card; // Return the created card
+
+        return card;
     }
-    
+
     private void configureLinkButton(JButton button) {
         button.setForeground(Color.BLUE);
         button.setCursor(new Cursor(Cursor.HAND_CURSOR));
@@ -140,5 +142,21 @@ public class ClientPanel extends JPanel {
         button.setBackground(Color.BLUE);
         button.setForeground(Color.WHITE);
         button.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+    }
+
+    private void configureSelectButton(JButton button) {
+        button.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        button.setFocusable(false);
+        button.setBorderPainted(false);
+        button.setContentAreaFilled(false);
+        button.setBackground(Color.BLUE);
+        button.setForeground(Color.WHITE);
+        button.setFont(button.getFont().deriveFont(Font.BOLD));
+        button.setOpaque(true);
+        button.setBorder(BorderFactory.createEmptyBorder(7, 0, 7, 0));
+    }
+
+    public Pizzeria getPizzeria() {
+        return pizzeria;
     }
 }
---
+++ src/main/java/com/rapizza/listeners/SelectButtonListener.java
@@ -0,0 +1,14 @@
+package com.rapizza.listeners;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class SelectButtonListener implements ActionListener {
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        
+
+    }
+    
+}
