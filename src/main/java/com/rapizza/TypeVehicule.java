93d878b03a6f9df3f2b74e0c4cd74cfdf129c111
Massa Timothée <timothee.massa.perso@gmail.com>
2024-04-16 14:46:17 +0200

    add conversion method

--- src/main/java/com/rapizza/TypeVehicule.java
+++ src/main/java/com/rapizza/TypeVehicule.java
@@ -4,5 +4,16 @@ package com.rapizza;
  */
 public enum TypeVehicule {
     voiture,
-    moto
+    moto;
+
+    public String toString() {
+        switch (this) {
+            case voiture:
+                return "Voiture";
+            case moto:
+                return "Moto";
+            default:
+                return "Voiture";
+        }
+    }
 }
